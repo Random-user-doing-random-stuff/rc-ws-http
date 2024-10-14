b83c5ac4b236e1db75b5dd1e28e04c97a8181ffa
Random-user-doing-random-stuff <118290682+Random-user-doing-random-stuff@users.noreply.github.com>
2024-10-14 10:13:43 +0000

    a

--- api/routes/user/login.js
+++ api/routes/user/login.js
@@ -1,9 +1,7 @@
 const { app } = require("../../router");
-const {User} = require('../../../db/schemas/user')
+
 const login = (req, res) => {
   console.log(req.body);
   res.send("sup");
-}
+};
 module.exports = login;
-
-User
